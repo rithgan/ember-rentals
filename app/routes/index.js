80f530a20844fcef641fdcfa9690c0e8a28d9262
=rithgan <=rithikgandhi1989@gmail.com>
2020-12-22 16:45:47 +0530

    adapting server data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,23 @@
 import Route from "@ember/routing/route";
 
+const COMMUNITY_CATEGORIES = ["Condo", "Townhouse", "Apartment"];
 export default class IndexRoute extends Route {
   async model() {
     let res = await fetch("/api/rentals.json");
-    let parsed = await res.json();
-    return parsed;
+    // let parsed = await res.json();
+    // return parsed;
+
+    let { data } = await res.json();
+
+    return data.map((model) => {
+      let type;
+      let { attributes } = model;
+
+      if (COMMUNITY_CATEGORIES.includes(attributes.category))
+        type = "Community";
+      else type = "Standalone";
+
+      return { type, ...attributes };
+    });
   }
 }
